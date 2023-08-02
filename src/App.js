109d2afc6246f40c288a251ea17618d952f9b86e
enycezchris <enycezchris@gmail.com>
2023-08-02 17:57:55 -0400

    changed axios get request

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import { Container, Title, Description, List, ListItem } from "./AppStyles";
-
-// const App = () => {
-//   const [items, setItems] = useState([]);
-//   // useEffect(() => {
-//   //   axios.get("http://3.142.12.33:3001/").then((response) => {
-//   //     setItems(response.data);
-//   //   });
-//   // }, []);
-
-//   return (
-//     <Container>
-//       {items?.map((item) => {
-//         return (
-//           <List key={item.id}>
-//             <ListItem>
-//               <Title>{item.title}</Title>
-//               <Description>{item.description}</Description>
-//             </ListItem>
-//           </List>
-//         );
-//       })}
-//     </Container>
-//   );
-// };
-
-// export default App;
-
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Title, Description, List, ListItem } from "./AppStyles";
 
 const App = () => {
-  return <div>App</div>;
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios.get("http://3.142.12.33:3001/").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
+  return (
+    <Container>
+      {items?.map((item) => {
+        return (
+          <List key={item.id}>
+            <ListItem>
+              <Title>{item.title}</Title>
+              <Description>{item.description}</Description>
+            </ListItem>
+          </List>
+        );
+      })}
+    </Container>
+  );
 };
 
 export default App;
